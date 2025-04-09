4b7b493ff267a2880a1552684de6afec56ed708d
JagdeepSingh13 <jagdeeps3105@gmail.com>
2025-04-09 15:43:03 +0530

    sidebar sheet set-up

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,11 +1,44 @@
 "use client";
 
+import { MenuIcon } from "lucide-react";
 import NewDocumentButton from "./NewDocumentButton";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 
 const Sidebar = () => {
+  const menuOptions = (
+    <>
+      <NewDocumentButton />
+    </>
+  );
+
   return (
     <div className="p-2 md:p-5 bg-gray-200 relative">
-      <NewDocumentButton />
+      <div className="md:hidden">
+        <Sheet>
+          <SheetTrigger>
+            <MenuIcon className="p-2 hover:opacity-30 rounded-lg" size={40} />
+          </SheetTrigger>
+          <SheetContent side="left">
+            <SheetHeader>
+              <SheetTitle>Menu</SheetTitle>
+              <div>{menuOptions}</div>
+              <SheetDescription>
+                This action cannot be undone. This will permanently delete your
+                account and remove your data from our servers.
+              </SheetDescription>
+            </SheetHeader>
+          </SheetContent>
+        </Sheet>
+      </div>
+
+      <div className="hidden md:inline">{menuOptions}</div>
     </div>
   );
 };
