b04af7fe4119df721f6707a1e2aef16b71179e33
JagdeepSingh13 <jagdeeps3105@gmail.com>
2025-04-09 10:23:36 +0530

    clerk auth, header, sidebar layout

---
+++ components/Sidebar.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import NewDocumentButton from "./NewDocumentButton";
+
+const Sidebar = () => {
+  return (
+    <div className="p-2 md:p-5 bg-gray-200 relative">
+      <NewDocumentButton />
+    </div>
+  );
+};
+
+export default Sidebar;
