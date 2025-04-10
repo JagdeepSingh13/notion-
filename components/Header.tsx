ef4427fe39243b1feef0e1c89ff960ad635386af
JagdeepSingh13 <jagdeeps3105@gmail.com>
2025-04-10 15:32:21 +0530

    breadcrumbs added

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,6 +7,7 @@ import {
   UserButton,
 } from "@clerk/clerk-react";
 import { useUser } from "@clerk/nextjs";
+import Breadcrumbs from "./Breadcrumbs";
 
 const Header = () => {
   const { user } = useUser();
@@ -19,6 +20,8 @@ const Header = () => {
           {`'s`} Space
         </h1>
       )}
+      {/* Breadcrumbs */}
+      <Breadcrumbs />
       <div>
         <SignedOut>
           <SignInButton />
