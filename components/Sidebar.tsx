d46c60d6a452adf7c5a2385790136922bb742274
JagdeepSingh13 <jagdeeps3105@gmail.com>
2025-04-10 10:31:41 +0530

    fetch shared with me docs

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -107,6 +107,18 @@ const Sidebar = () => {
           </>
         )}
       </div>
+
+      {/* Shared with me Docs. */}
+      {groupedData.editor.length > 0 && (
+        <>
+          <h2 className="text-gray-500 font-semibold text-sm">
+            Shared With Me
+          </h2>
+          {groupedData.editor.map((doc) => (
+            <SidebarOptions key={doc.id} id={doc.id} href={`/doc/${doc.id}`} />
+          ))}
+        </>
+      )}
     </>
   );
 
