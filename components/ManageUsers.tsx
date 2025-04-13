98bc9016f4f53d264a8a194ae7fbc904305b1df4
JagdeepSingh13 <jagdeeps3105@gmail.com>
2025-04-13 11:16:41 +0530

    manage and delete users in document

--- components/ManageUsers.tsx
+++ components/ManageUsers.tsx
@@ -10,7 +10,6 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-
 import { Button } from "./ui/button";
 import { FormEvent, useState, useTransition } from "react";
 import { toast } from "sonner";
@@ -20,6 +19,7 @@ import { useRoom } from "@liveblocks/react/suspense";
 import { useCollection } from "react-firebase-hooks/firestore";
 import { collectionGroup, query, where } from "firebase/firestore";
 import { db } from "@/firebase";
+import { removeUserFromDocument } from "@/actions/actions";
 
 function ManageUsers() {
   const { user } = useUser();
@@ -35,7 +35,20 @@ function ManageUsers() {
   if (!room.id) {
     return <div>Loading room data...</div>;
   }
-  const handleDelete = (userId: string) => {};
+  const handleDelete = (userId: string) => {
+    startTransition(async () => {
+      if (!user) return;
+
+      const { success } = await removeUserFromDocument(room.id, userId);
+      console.log(room.id);
+
+      if (success) {
+        toast.success("User romoved from room successfully");
+      } else {
+        toast.error("Failed to remove user from room");
+      }
+    });
+  };
 
   return (
     <Dialog open={isOpen} onOpenChange={setIsOpen}>
@@ -50,7 +63,8 @@ function ManageUsers() {
           </DialogDescription>
         </DialogHeader>
         <hr className="my-2" />
-        <div>
+
+        <div className="flex flex-col space-y-2">
           {/* Users in room */}
           {usersInRoom?.docs.map((doc) => (
             <div
@@ -62,6 +76,20 @@ function ManageUsers() {
                   ? `You (${doc.data().userId})`
                   : doc.data().userId}
               </p>
+              <div className="flex items-center gap-2">
+                <Button variant="outline">{doc.data().role}</Button>
+                {isOwner &&
+                  doc.data().userId !== user?.emailAddresses[0].toString() && (
+                    <Button
+                      variant="destructive"
+                      onClick={() => handleDelete(doc.data().userId)}
+                      disabled={isPending}
+                      size="sm"
+                    >
+                      {isPending ? "Removing..." : "X"}
+                    </Button>
+                  )}
+              </div>
             </div>
           ))}
         </div>
